7ba124f8c4bc3c2fb4461212fa87db2970bfa6e1
Ksenya <kseni-apple@mail.ru>
2017-09-26 11:48:39 +0300

    Задание №3

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.annotations.Test;
+
+public class PointTests {
+@Test
+  public void tests(){
+  Point p1 = new Point();
+  Point p2 = new Point();
+  p1.x=2;
+  p1.y=-4;
+  p2.x=-5;
+  p2.y=3;
+
+assert p1.distance(p1,p2)== 9.899494936611665;
+  }
+}
