569369cbd5f91acf64c208509e7b7447846eb94d
Ksenya <kseni-apple@mail.ru>
2017-09-26 10:14:40 +0300

    Задание№2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -2,7 +2,27 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgramm {
 
-public static void main(String[] args){
-System.out.println("Hello, world!");
-}
+  public static void main(String[] args) {
+    Point p1 = new Point();
+    Point p2 = new Point();
+   p1.x=2;
+   p1.y=-4;
+   p2.x=-5;
+   p2.y=3;
+    System.out.println("Hello");
+    System.out.println(distance(p1,p2));
+    System.out.println(p1.distance(p1,p2));
+
+  }
+
+  public static double distance(Point p1, Point p2) {
+
+    double d;
+
+    d =Math.sqrt(Math.pow(p2.x-p1.x,2)+Math.pow(p2.y-p1.y,2));
+    return d;
+  }
+
+
+
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+
+double x,y;
+
+  public double distance(Point p1, Point p2){
+   double d;
+
+
+  d =Math.sqrt(Math.pow(p2.x-p1.x,2)+Math.pow(p2.y-p1.y,2));
+    return d;
+  }
+
+}
