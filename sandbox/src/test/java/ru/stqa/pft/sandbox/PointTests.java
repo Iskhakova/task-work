80709edef2f39aa0353f891904609eaa651f3d71
Ksenya <kseni-apple@mail.ru>
2017-09-28 14:22:51 +0300

    Gitignor+ конструктора класса Point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -6,12 +6,12 @@ import org.testng.annotations.Test;
 public class PointTests {
 @Test
   public void tests(){
-  Point p1 = new Point();
-  Point p2 = new Point();
-  p1.x=2;
-  p1.y=-4;
-  p2.x=-5;
-  p2.y=3;
+  Point p1 = new Point(2,-4);
+  Point p2 = new Point(-5,3);
+  //p1.x=2;
+ // p1.y=-4;
+ // p2.x=-5;
+ // p2.y=3;
 
   Assert.assertEquals(p1.distance(p1,p2),9.899494936611665);
   }
